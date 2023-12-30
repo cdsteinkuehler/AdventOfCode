1966bc44c5f009d5a1fd19c1c995e091f9bd7876
Charles Steinkuehler <cstein@vizrt.com>
2023-12-30 11:34:13 -0600

    Day 25
    Tricky, but a crafty algoritm made it quick!
    
    Signed-off-by: Charles Steinkuehler <cstein@vizrt.com>

---
+++ 2023/day25/script1.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+function count () {
+	R=""
+	RX=0
+	RL=0
+	C=0
+	for N in ${A} ; do
+		X=${AC[$N]:-0}
+		# Track the node with the most external connections..
+		if (( X > RX )) ; then
+			R=$N
+			RX=$X
+			RL=${#G[$N]}
+		# ...and the most neighbors
+		elif (( X == RX && RL < ${#G[$N]} )) ; then
+			R=$N
+			RX=$X
+			RL=${#G[$N]}
+		fi
+		let C+=$X
+	done
+	let i+=1
+}
+
+function remove () {
+	for N in ${G[$1]} ; do
+		let AC[$N]=${AC[$N]:-0}+1
+	done
+}
+
+function separate () {
+	A="${!G[@]}"
+	B=""
+	C=0
+	i=0
+
+	count
+	echo "R: $R, C: $C"
+
+	while (( $C != 3 )) ; do
+		A="${A//$R/}"
+		B="$B $R"
+		remove $R
+		count
+		echo "$i R: $R, C: $C"
+	done
+}
+
+declare -A G AC
+
+LN=0
+
+{ cat ${1:-input} ; echo ; } |
+while read -r LINE ; do
+	if [ -z "$LINE" ] ; then
+
+		for N in "${!G[@]}" ; do
+			echo "Node $N :${G[$N]}"
+		done
+
+		separate
+		echo "A: $A"
+		echo "B: $B"
+		set -- $A
+		AN=$#
+		set -- $B
+		BN=$#
+		echo "AN: $AN BN: $BN"
+		let T=AN*BN
+		echo "Answer: $T"
+
+		exit
+	fi
+
+	L[LN]="$LINE"
+	set -- $LINE
+	N=${1/:/}
+	shift
+	while (( $# > 0 )) ; do
+		G[$N]="${G[$N]} $1"
+		G[$1]="${G[$1]} $N"
+		shift
+	done
+
+	let LN+=1
+
+done
+
